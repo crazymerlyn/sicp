34f073c0efa798ee2765f86f7b881f13b0b3ce97
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-09-10 17:35:50 +0530

    Solved 3.3 and 3.4 - Improving the account with passwords and cops

--- objects.scm
+++ objects.scm
@@ -14,3 +14,29 @@
        (else
          (set! count (+ count 1))
          (f input))))))
+
+(define (make-account balance password)
+  (define failed-attemps 0)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (define (dispatch pass m)
+    (if (eq? pass password)
+        (begin
+          (set! failed-attemps 0)
+          (cond ((eq? m 'withdraw) withdraw)
+                ((eq? m 'deposit) deposit)
+                (else (error "Unkown request -- MAKE_ACCOUNT"
+                             m))))
+        (begin
+          (set! failed-attemps (+ 1 failed-attemps))
+          (if (>= failed-attemps 7)
+              (call-the-cops)
+              (error "Incorrect password")))))
+  dispatch)
+
