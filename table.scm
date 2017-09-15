b5d0806f0cee6820b47662519680ca68dcd37e62
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-09-15 09:09:58 +0530

    Solved 3.25 - multidimensional table

--- table.scm
+++ table.scm
@@ -29,3 +29,33 @@
            ((eq? m 'insert-proc!) insert!)
            (else (error "Unknown operation -- TABLE" m))))
    dispatch))
+
+(define (make-generic-table)
+  (let ((local-table (list *table*)))
+   (define (lookup keys)
+     (define (lookup1 table keys)
+       (if (null? keys)
+           (cdr table)
+           (let ((record (assoc (car keys) (cdr table))))
+            (if record
+                (lookup1 record (cdr keys))
+                false))))
+     (lookup1 local-table keys))
+   (define (insert! keys value)
+     (define (insert1! table keys)
+       (if (null? keys)
+           (set-cdr! table value)
+           (let ((record (assoc (car keys) (cdr table))))
+            (if record
+                (insert1! record (cdr keys))
+                (begin
+                  (set-cdr! table 
+                            (cons (cons (car keys) '())
+                                  (cdr table)))
+                  (insert1! (cadr table) (cdr keys)))))))
+     (insert1! local-table keys))
+   (define (dispatch m)
+     (cond ((eq? m 'lookup-proc) lookup)
+           ((eq? m 'insert-proc!) insert!)
+           (else (error "Unknown operation generic TABLE" m))))
+   dispatch))
