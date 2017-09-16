cb534d2709280db12a08cb4f21fcd4f535958765
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-09-17 03:11:07 +0530

    Simulating digital circuits

--- circuits.scm
+++ circuits.scm
@@ -1,3 +1,36 @@
+(define (call-each procedures)
+  (if (null? procedures)
+      'done
+      (begin
+        ((car procedures))
+        (call-each (cdr procedures)))))
+
+(define (make-wire)
+  (let ((signal-value 0) (action-procedures '()))
+   (define (set-my-signal! new-value)
+     (if (not (= signal-value new-value))
+         (begin (set! signal-value new-value)
+                (call-each action-procedures))
+         'done))
+   (define (accept-action-procedure! proc)
+     (set! action-procedures (cons proc action-procedures))
+     (proc))
+   (define (dispatch m)
+     (cond ((eq? m 'get-signal) signal-value)
+           ((eq? m 'set-signal!) set-my-signal!)
+           ((eq? m 'add-action!) accept-action-procedure!)
+           (else (error "Unknown operation -- WIRE" m))))
+   dispatch))
+
+(define (get-signal wire)
+  (wire 'get-signal))
+
+(define (set-signal! wire new-value)
+  ((wire 'set-signal!) new-value))
+
+(define (add-action! wire proc)
+  ((wire 'add-action!) proc))
+
 (define (invert input output)
   (define (invert-input)
     (let ((new-value (logical-not (get-signal input))))
@@ -12,9 +45,23 @@
         ((= s 1) 0)
         (else (error "Invalid signal" s))))
 
+(define (logical-and . elems)
+  (if (null? elems)
+      1
+      (if (= 0 (car elems))
+          0
+          (logical-or (cdr elems)))))
+
+(define (logical-or . elems)
+  (if (null? elems)
+      0
+      (if (= 1 (car elems))
+          1
+          (apply logical-or (cdr elems)))))
+
 (define (and-gate a1 a2 output)
   (define (and-action-procedure)
-    (let ((new-value (and (get-signal a1) (get-signal a2))))
+    (let ((new-value (logical-and (get-signal a1) (get-signal a2))))
      (after-delay and-gate-delay
                   (lambda ()
                     (set-signal! output new-value)))))
@@ -24,7 +71,7 @@
 
 (define (or-gate a1 a2 output)
   (define (or-action-procedure)
-    (let ((new-value (or (get-signal a1) (get-signal a2))))
+    (let ((new-value (logical-or (get-signal a1) (get-signal a2))))
      (after-delay or-gate-delay
                   (lambda ()
                     (set-signal! output new-value)))))
@@ -72,3 +119,91 @@
           (let ((c1 (make-wire)))
            (full-adder (car as) (car bs) c (car sums) c1)
            (ripple-adder (cdr as) (cdr bs) (cdr sums) c1)))))
+
+(define (after-delay delay action)
+  (add-to-agenda! the-agenda
+                  (+ delay (current-time the-agenda))
+                  action))
+
+(define (propagate)
+  (if (empty-agenda? the-agenda)
+      'done
+      (let ((first-item (first-agenda-item the-agenda)))
+       (first-item)
+       (remove-first-agenda-item! the-agenda)
+       (propagate))))
+
+
+(define (probe name wire)
+  (add-action! wire
+               (lambda ()
+                 (newline)
+                 (display " ")
+                 (display name)
+                 (display (current-time the-agenda))
+                 (display " New-value = ")
+                 (display (get-signal wire))
+                 )))
+
+(define (make-time-segment time queue)
+  (cons time queue))
+
+(define (segment-time s) (car s))
+(define (segment-queue s) (cdr s))
+
+(define (make-agenda) (list 0))
+(define (current-time agenda) (car agenda))
+(define (set-current-time! agenda time)
+  (set-car! agenda time))
+(define (segments agenda) (cdr agenda))
+(define (set-segments! agenda segments)
+  (set-cdr! agenda segments))
+(define (first-segment agenda) (car (segments agenda)))
+(define (rest-segments agenda) (cdr (segments agenda)))
+(define (empty-agenda? agenda)
+  (null? (segments agenda)))
+
+(define (add-to-agenda! agenda time action)
+  (define (belongs-before? segments)
+    (or (null? segments)
+        (< time (segment-time (car segments)))))
+  (define (make-new-time-segment time action)
+    (let ((q (make-queue)))
+     (insert-queue! q action)
+     (make-time-segment time q)))
+  (define (add-to-segments! segments)
+    (if (= (segment-time (car segments)) time)
+        (insert-queue! (segment-queue (car segments))
+                       action)
+        (let ((rest (cdr segments)))
+         (if (belongs-before? rest)
+             (set-cdr!
+               segments
+               (cons (make-new-time-segment time action)
+                     (cdr segments)))
+             (add-to-segments! rest)))))
+  (let ((segments (segments agenda)))
+   (if (belongs-before? segments)
+       (set-segments!
+         agenda
+         (cons (make-new-time-segment time action)
+               segments))
+       (add-to-segments! segments))))
+
+(define (remove-first-agenda-item! agenda)
+  (let ((q (segment-queue (first-segment agenda))))
+   (delete-queue! q)
+   (if (empty-queue? q)
+       (set-segments! agenda (rest-segments agenda)))))
+
+(define (first-agenda-item agenda)
+  (if (empty-agenda? agenda)
+      (error "Agenda is empty -- FIRST-AGENDA-ITEM")
+      (let ((first-seg (first-segment agenda)))
+       (set-current-time! agenda (segment-time first-seg))
+       (front-queue (segment-queue first-seg)))))
+
+(define the-agenda (make-agenda))
+(define inverter-delay 2)
+(define and-gate-delay 3)
+(define or-gate-delay 5)
