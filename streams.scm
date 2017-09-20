a57e691bd4d4172693d7d949257aa21e5eaa2c97
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-09-20 11:23:39 +0530

    Solved 3.50 - Implement stream map

---
+++ streams.scm
@@ -0,0 +1,30 @@
+(define (stream-car stream) (car stream))
+(define (stream-cdr stream) (force (cdr stream)))
+
+(define the-empty-stream '())
+(define stream-null? null?)
+
+(define (stream-enumerate-interval low high)
+  (if (< low high)
+      the-empty-stream
+      (cons-stream
+        low
+        (stream-enumerate-interval (+ low 1) high))))
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter pred
+                                     (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+        (apply proc (map stream-car argstreams))
+        (apply stream-map
+               (cons proc (map stream-cdr argstreams))))))
+
+
