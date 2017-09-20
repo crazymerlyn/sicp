fe1712d3a31009fd9b52a1e85a72e8d4584980eb
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-09-21 00:14:20 +0530

    Solved 3.60 - Implement multiplying two series

--- streams.scm
+++ streams.scm
@@ -94,3 +94,9 @@
 (define sine-series
   (cons-stream 0 (integrate-series cosine-series)))
 
+(define (mul-series s1 s2)
+  (cons-stream (* (stream-car s1) (stream-car s2))
+               (add-streams (stream-map (multiply (stream-car s1))
+                                        (stream-cdr s2))
+                            (mul-series (stream-cdr s1) s2))))
+
