2d237c36882724618c2debe90abdaab8359b4e38
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-09-21 17:36:47 +0530

    Solved 3.67 - Implement all-pairs

--- streams.scm
+++ streams.scm
@@ -152,3 +152,26 @@
                           (+ s0 (* -2 s1) s2)))
                  (euler-transform (stream-cdr s)))))
 
+(define (interleave s1 s2)
+  (if (stream-null? s1)
+      s2
+      (cons-stream (stream-car s1)
+                   (interleave s2
+                               (stream-cdr s1)))))
+
+(define (pairs s1 s2)
+  (cons-stream
+    (list (stream-car s1) (stream-car s2))
+    (interleave
+      (stream-map (lambda (x) (list (stream-car s1) x))
+                  (stream-cdr s2))
+      (pairs (stream-cdr s1) (stream-cdr s2)))))
+
+(define (all-pairs s1 s2)
+  (cons-stream
+    (list (stream-car s1) (stream-car s2))
+    (interleave
+      (stream-map (lambda (x) (list (stream-car s1) x))
+                  (stream-cdr s2))
+      (all-pairs (stream-cdr s1) s2))))
+
