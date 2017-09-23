2245f5d40463f08e50f65b33d94a961bb9003f60
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-09-23 17:24:13 +0530

    Implement primitive procedures and global env

--- evaluator.scm
+++ evaluator.scm
@@ -345,3 +345,37 @@
     (if (null? binding)
         (add-binding-to-frame! var val frame)
         ((binding 'set-value!) val))))
+
+
+(define (primitive-procedure? proc)
+  (tagged-list? proc 'primitive))
+
+(define (primitive-implementation proc) (cadr proc))
+
+(define (primitive-procedures)
+  (list (list 'car car)
+        (list 'cdr cdr)
+        (list 'cons cons)
+        (list 'null? null?)))
+
+(define (primitive-procedure-names)
+  (map car primitive-procedures))
+(define (primitive-procedure-objects)
+  (map (lambda (proc) (list 'primitive (cadr proc)))
+       primitive-procedures))
+
+(define (apply-primitive-procedure proc args)
+  (apply (primitive-implementation proc) args))
+
+
+(define (setup-environment)
+  (let ((initial-env
+          (extend-environment (primitive-procedure-names)
+                              (primitive-procedure-objects)
+                              the-empty-environment)))
+    (define-variable! 'true true initial-env)
+    (define-variable! 'false false initial-env)
+    initial-env))
+
+(define the-global-environment (setup-environment))
+
